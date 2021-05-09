e183ce66545e1db85dba01f8098a5a34b6bdc3bf
Andrew Kamand <andrewkamand@gmail.com>
2021-05-09 14:05:22 -0700

    Wrote test2 to test if boundary value of 8 chars correctly returns true

--- tests.py
+++ tests.py
@@ -3,10 +3,17 @@ from check_pwd import check_pwd
 
 
 class MyTestCase(unittest.TestCase):
+    # checks if empty string returns false
     def test1(self):
         pw = ''
         self.assertFalse(check_pwd(pw), msg='Does not meet the requirements '
                                             '(num={})'.format(pw))
+
+    # checks boundary values, if string of 8 chars returns true
+    def test2(self):
+        pw = 'Ab34567$'
+        self.assertTrue(check_pwd(pw), msg='Does not meet the requirements '''
+                                           '(num={})'.format(pw))
 
 
 if __name__ == '__main__':
