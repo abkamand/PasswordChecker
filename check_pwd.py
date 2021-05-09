9bae47f5251cfcca64a898b5fef1d7d264057cf1
Andrew Kamand <andrewkamand@gmail.com>
2021-05-09 14:34:52 -0700

    Changed check_pwd by adding a new if conditional. Created a counter for lowercase chars. If a char in passed string, pw, is lowercase, increment this counter by 1. If said counter is 0 after parsing through string, return False, as that means there is no lowercase char. Tests passed (notably test4).

--- check_pwd.py
+++ check_pwd.py
@@ -1,7 +1,15 @@
 def check_pwd(pw):
+    count_lower = 0
+
     pw_length = len(pw)
 
     if (pw_length > 20 or pw_length < 8):
+        return False
+
+    for char in pw:
+        if char.islower():
+            count_lower += 1
+    if count_lower == 0:
         return False
 
     return True
