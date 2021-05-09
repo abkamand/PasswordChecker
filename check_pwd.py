ae2fa010c3e6a7bf370ab2722c97d4d97fc81ed4
Andrew Kamand <andrewkamand@gmail.com>
2021-05-09 14:41:11 -0700

    Tweaked check_pwd by adding a new count_upper. Similar approach to our count_lower. If a char in passed pw string is uppercase, increment count_upper by 1. If after checking all chars, count_upper is 0, return False. All tests passed (notably test5)! We now successfully parse the pw string and check for a lowercase and uppercase char.

--- check_pwd.py
+++ check_pwd.py
@@ -1,5 +1,6 @@
 def check_pwd(pw):
     count_lower = 0
+    count_upper = 0
 
     pw_length = len(pw)
 
@@ -9,7 +10,9 @@ def check_pwd(pw):
     for char in pw:
         if char.islower():
             count_lower += 1
-    if count_lower == 0:
+        if char.isupper():
+            count_upper += 1
+    if count_lower == 0 or count_upper == 0:
         return False
 
     return True
