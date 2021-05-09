77ceafd0860fd86e27536d8bcc92572b6168f626
Andrew Kamand <andrewkamand@gmail.com>
2021-05-09 13:54:31 -0700

    Starter code - wrote test1 to make sure test fails when asserting false for a skeleton check_pwd that starts with simply 'return True'

--- check_pwd.py
+++ check_pwd.py
@@ -1,1 +1,2 @@
-
+def check_pwd(pw):
+    return True
--- tests.py
+++ tests.py
@@ -1,1 +1,13 @@
+import unittest
+from check_pwd import check_pwd
 
+
+class MyTestCase(unittest.TestCase):
+    def test1(self):
+        pw = ''
+        self.assertFalse(check_pwd(pw), msg='Does not meet the requirements '
+                                            '(num={})'.format(pw))
+
+
+if __name__ == '__main__':
+    unittest.main()
